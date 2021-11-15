4e2599aed07bfe9a07e5bd9275eb3969483fd744
Marcio Cruz de Almeida <marciocadev@gmail.com>
2021-11-15 10:46:59 -0300

    create text eventbus

--- lib/aws-texts-stack.ts
+++ lib/aws-texts-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 // import * as sqs from '@aws-cdk/aws-sqs';
+import { EventBus } from '@aws-cdk/aws-events';
 
 export class AwsTextsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const eventBus = new EventBus(this, 'TextEventBus', { eventBusName: 'TextEventBus' });
+    console.log(eventBus.eventBusName);
 
     // The code that defines your stack goes here
 
